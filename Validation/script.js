76bcf170435fd45c05b32f604ed7eb150357363e
BananaLimez <125242556+BananaLimez@users.noreply.github.com>
2025-08-04 15:29:45 +0700

    Add files via upload

---
+++ Validation/script.js
@@ -0,0 +1,72 @@
+// Selecting form and input elements
+const form = document.querySelector("form");
+const passwordInput = document.getElementById("password");
+const passwordInput2 = document.getElementById("password2");
+const passToggleBtn = document.getElementById("pass-toggle-btn");
+
+// Function to display error messages
+const showError = (field, errorText) => {
+    field.classList.add("error");
+    const errorElement = document.createElement("small");
+    errorElement.classList.add("error-text");
+    errorElement.innerText = errorText;
+    field.closest(".form-group").appendChild(errorElement);
+}
+
+// Function to handle form submission
+const handleFormData = (e) => {
+    e.preventDefault();
+
+    // Retrieving input elements
+    const fullnameInput = document.getElementById("fullname");
+    const dateInput = document.getElementById("date");
+    const genderInput = document.getElementById("gender");
+
+    // Getting trimmed values from input fields
+    const fullname = fullnameInput.value.trim();
+    const password = passwordInput.value.trim();
+    const password2 = passwordInput2.value.trim();
+    const date = dateInput.value;
+    const gender = genderInput.value;
+
+
+    // Clearing previous error messages
+    document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
+    document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
+
+    // Performing validation checks
+    if (fullname === "") {
+        showError(fullnameInput, "Enter your full name");
+    }
+    if (password === "") {
+        showError(passwordInput, "Enter your password");
+    }
+    if (password2 === "") {
+        showError(passwordInput2, "Enter your password");
+    }
+    if (password2 != password) {
+        showError(passwordInput2, "Wrong password");
+    }
+    if (date === "") {
+        showError(dateInput, "Select your date of birth");
+    }
+    if (gender === "") {
+        showError(genderInput, "Select your gender");
+    }
+
+    // Checking for any remaining errors before form submission
+    const errorInputs = document.querySelectorAll(".form-group .error");
+    if (errorInputs.length > 0) return;
+
+    // Submitting the form
+    form.submit();
+}
+
+// Toggling password visibility
+passToggleBtn.addEventListener('click', () => {
+    passToggleBtn.className = passwordInput.type === "password" ? "fa-solid fa-eye-slash" : "fa-solid fa-eye";
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+});
+
+// Handling form submission event
+form.addEventListener("submit", handleFormData);
